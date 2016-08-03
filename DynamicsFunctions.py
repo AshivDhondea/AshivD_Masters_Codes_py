87a7b8b1fd013ecb19d052cea09c6ffa6471397e
AshivDhondea <AshivDhondea@users.noreply.github.com>
2016-08-03 15:18:48 +0200

    Update DynamicsFunctions.py

--- DynamicsFunctions.py
+++ DynamicsFunctions.py
@@ -1,3 +1,4 @@
+## DynamicsFunctions.py
 # ------------------------- #
 # Description:
 # A collection of functions which implement dynamics-related functions for orbiting objects.
@@ -13,17 +14,28 @@
 # 15 July 2016: edited yesterday's function.
 # 16 July 2016: edited the function fnMoment_DE to be integrated by fnRK4_moment(f, dt, x,t,L,Q)
 # 19 July 2016: edited the function fn_Create_Concatenated_Block_Diag_Matrix. Probably a better implementation.
+# 22 July 2016: removed the function fn_Create_Concatenated_Block_Diag_Matrix and moved it to MathsFunctions.py
+# 22 July 2016: cleaned up all functions so that code is reused. import MathsFunctions for Cholesky decomposition of positive semidefinite matrices.
+# 24 July 2016: created the function fn_CWPA_Discretized_Covariance_Matrix(dt,intensity,acc) which generates the discretized process noise covariance matrix for NCAM-CWPA models.
+#
+# ------------------------- #
+## References
+# 1. Tracking Filter Engineering, Norman Morrison. 2013
+# 2. Estimation with applications to tracking and navigation, Bar Shalom, Li, Kirubarajan. 2001
 # ------------------------- #
 # Import libraries
 import numpy as np
 from numpy import linalg
 import scipy
+import math
 # Ashiv's own libraries
 import AstroConstants as AstCnst
 import Num_Integ as ni
+import MathsFunctions as MathsFn
 # -------------------------------------------------------------------- #
 def fnKepler_J2(t,X):
     # emulates fnKepler_J2.m. 20 June 2016
+    # fnKepler_J2 implements the dynamics function of the J-2 perturbed two-body problem.
     r = np.linalg.norm(X[0:3]);
     Xdot = np.zeros([6],dtype=np.float64);
     Xdot[0:3] = X[3:6]; # dx/dt = xdot; dy/dt = ydot; dz/dt = zdot
@@ -36,46 +48,27 @@ def fnKepler_J2(t,X):
 
 def fnKepler_J2_augmented(t,X):
     # validated against the equivalent MATLAB function on 23 June 2016.
-    r = np.linalg.norm(X[0:3]);
+    # fnKepler_J2_augmented implements the dynamics function of the J-2 perturbed two-body problem
+    # augmented by the state transition matrix(STM) evaluated on the state vector X.
+    
     # state vector augmented by reshaped state transition matrix
     Xdot = np.zeros([6 + 6*6],dtype=np.float64);
-    Xdot[0:3] = X[3:6]; # dx/dt = xdot; dy/dt = ydot; dz/dt = zdot
-    expr = 1.5*AstCnst.J_2*(AstCnst.R_E/r)**2 ;
-    expr1 = -(AstCnst.mu_E/r**3)*(1 - expr*(5*((X[2]/r)**2) -1));
-    
-    Xdot[3] = expr1*X[0];
-    Xdot[4] = expr1*X[1];   
-    Xdot[5] = -(AstCnst.mu_E/r**3)*X[2]*(1-expr*(5*(X[2]/r)**2 -3));
+    Xdot[0:6] = fnKepler_J2(t,X);
     # The state transition matrix's elements are the last 36 elements in the input state vector
     Phi = np.reshape(X[6:6+6*6+1],(6,6)); # extract Phi matrix from the input vector X
     
     # Find matrix A (state sensitivity matrix)
-    # We start with a matrix of zeros and then add in the non-zero elements.
-    Amatrix = np.zeros([6,6],dtype=np.float64);
-    Amatrix[0,3]=1.0;
-    Amatrix[1,4]=1.0;
-    Amatrix[2,5]=1.0;
-
-    expr2 = 3*(AstCnst.mu_E/r**5)*(1 - 2.5*AstCnst.J_2*(AstCnst.R_E/r)**2*(7*(X[2]/r)**2) -1); # fixed
-    Amatrix[3,0] = expr1 + expr2*X[0]**2; 
-    Amatrix[3,1] = expr2*X[0]*X[1]; 
-    expr3 =  3*(AstCnst.mu_E/r**5)*(1 - 2.5*AstCnst.J_2*(AstCnst.R_E/r)**2*(7*(X[2]/r)**2) -3);
-    Amatrix[3,2] = expr3*X[0]*X[2];
-    
-    Amatrix[4,0] = Amatrix[3,1];
-    Amatrix[4,1] = expr1 + expr2*X[1]**2;
-    Amatrix[4,2] = expr3*X[1]*X[2];
-
-    Amatrix[5,0] = Amatrix[3,2];
-    Amatrix[5,1] = Amatrix[4,2];
-    Amatrix[5,2] =-(AstCnst.mu_E/r**3)*(1-expr*(5*(X[2]/r)**2 -3)) + 3*(AstCnst.mu_E/r**5)*(1 - 2.5*AstCnst.J_2*(AstCnst.R_E/r)**2*(7*(X[2]/r)**2) -5)*X[2]**2;
-    
+    Amatrix = fnJacobian_Kepler_J2(X);
     # The state transition matrix's differential equation.
     PhiDot = np.dot(Amatrix,Phi); # Amatrix is a time dependent variable.i.e. not constant
     Xdot[6:6+6*6+1] = np.reshape(PhiDot,6*6);
     return Xdot
 
 def fnGenerate_Nominal_Trajectory(Xdash,timevec):
+    # fnGenerate_Nominal_Trajectory generates a nominal trajectory Xstate over the time period timevec
+    # starting from Xdash.
+    # Dynamical model assumed: fnKepler_J_2_augmented.
+    # This function is required when the Gauss-Newton filter operates in iterative mode.
     dt = timevec[1]-timevec[0];
     Xstate = np.zeros([6+6*6,len(timevec)],dtype=np.float64);
     Xstate[0:6,0] = Xdash;
@@ -92,7 +85,7 @@ def fn_Generate_STM_polynom(zeta,nStates):
     # of degree (nStates-1) over a span of transition of zeta [s].
     # Polynomial models are a subset of the class of constant-coefficient linear DEs.
     # Refer to: Tracking Filter Engineering, Norman Morrison.
-    stm = np.eye(nStates,dtype=float);
+    stm = np.eye(nStates,dtype=np.float64);
     for yindex in range (0,nStates):
         for xindex in range (yindex,nStates): # STM is upper triangular
             stm[yindex,xindex] = np.power(zeta,xindex-yindex)/float(math.factorial(xindex-yindex));
@@ -101,19 +94,26 @@ def fn_Generate_STM_polynom(zeta,nStates):
 def fn_Generate_STM_polynom_3D(zeta,nStates,dimensionality):
     # fn_Generate_STM_polynom_3D generates the full state transition matrix for 
     # the required dimensionality.
-    stm = fn_Generate_STM_polynom(dt,nStates);
-    stm3 = fn_Create_Concatenated_Block_Diag_Matrix(stm,dimensionality-1);
+    stm = fn_Generate_STM_polynom(zeta,nStates);
+    stm3 = MathsFn.fn_Create_Concatenated_Block_Diag_Matrix(stm,dimensionality-1);
     return stm3;
 
-def fn_Create_Concatenated_Block_Diag_Matrix(R,stacklen):
-    # fn_Create_Concatenated_Block_Diag_Matrix creates a block diagonal matrix of size (stacklen) x (stacklen)
-    # whose diagonal blocks are copies of the matrix R.
-    ##L = [R]; 
-    ##for index in range (0,stacklen):
-    ##    L.append(R);
-    ##    ryn = scipy.linalg.block_diag(*L);
-    ryn =np.kron(np.eye(stacklen),R); # Edit: 19/07/2016: probably better idea than using a for loop.
-    return ryn
+def fn_CWPA_Discretized_Covariance_Matrix(dt,intensity,acc):
+    # fn_CWPA_Discretized_Covariance_Matrix generates the discretized covariance matrix in the
+    # Continuous Wiener Process Acceleration version of the Nearly Constant Acceleration Model (NCAM).
+    # 24 July 2016.
+    # Find continuous-time process covariance matrix, then discretize it.
+    qtilde = intensity*acc/np.sqrt(dt); # Choice of process noise intensity.
+    # The theory behind Discretized Continuous-time kinematic models is discussed thoroughly
+    # in Bar-Shalom, Rong Li, Kirubarajan: Estimation with applications to tracking and navigation.
+    Qc00 = np.power(dt,5)/20.0; # See discussion in section 6.2.3
+    Qc01 = np.power(dt,4)/8.0;
+    Qc02 = np.power(dt,3)/6.0;
+    Qc21 = np.square(dt)/2.0;
+    Qc = np.array([[Qc00,Qc01,Qc02],[Qc01,Qc02*2,Qc21],[Qc02,Qc21,dt]],dtype=np.float64);
+    Qd = np.diag(qtilde); # process noise covariance matrix for the 1D case
+    Qd3 = np.kron(Qd,Qc); # Generate the corresponding matrix for the 3D case
+    return Qd3
 # -------------------------------------------------------------------------------------------------------------- #
 ## fnMoment_DE implements the DEs for propagation of the mean and covariance.
     """ Based on "Various ways to compute the continuous-discrete extended Kalman filter"
@@ -127,62 +127,40 @@ def fn_Create_Concatenated_Block_Diag_Matrix(R,stacklen):
       year={2012},
       publisher={IEEE}
     } """
-def fnMoment_DE(t,X):
+def fnMoment_DE(t,X,Q,L):
     # 14 July 2016: moment propagation differential equations. Based on Frogerais 2012.
-    # Function called by CD-EKF MC-RK4 (fnCD_EKF_predict_MC_RK4)
-    # Code for computing mean state vector and Jacobian matrix are obtained from the function fnKepler_J2_augmented
+    # Function called by numerical integration method called by CD-EKF MC-RK4 (fnCD_EKF_predict_MC_RK4)
+    # Edit: 28/07/16: added Q and L to the arguments list.
 
     # t is the time.
     # X is the mean state vector augmented by the reshaped covariance matrix.
     # L is the dispersion matrix.
     # Q is the process noise covariance matrix.
-    L = np.zeros([6,3],dtype=np.float64);
-    L[3,0] = 1.0; L[4,1]=1.0;L[5,2]=1.0;
-    Q = np.diag([2.4064e-6,2.4064e-6,2.4064e-6]);
-    
-    r = np.linalg.norm(X[0:3]);  
-
-    # state vector augmented by reshaped state transition matrix
+        
+    # state vector
     Xdot = np.zeros([6 + 6*6],dtype=np.float64);
-    Xdot[0:3] = X[3:6]; # dx/dt = xdot; dy/dt = ydot; dz/dt = zdot
-    expr = 1.5*AstCnst.J_2*(AstCnst.R_E/r)**2 ;
-    expr1 = -(AstCnst.mu_E/r**3)*(1 - expr*(5*((X[2]/r)**2) -1));
-    
-    Xdot[3] = expr1*X[0];
-    Xdot[4] = expr1*X[1];   
-    Xdot[5] = -(AstCnst.mu_E/r**3)*X[2]*(1-expr*(5*(X[2]/r)**2 -3));
-
+    Xdot[0:6] = fnKepler_J2(t,X);
     # Extract covariance matrix from state vector.
-    P = np.reshape(X[6:6+6*6+1],(6,6));
-    # Find matrix A (state sensitivity matrix)
-    # We start with a matrix of zeros and then add in the non-zero elements.
-    Amatrix = np.zeros([6,6],dtype=np.float64);
-    Amatrix[0,3]=1.0;
-    Amatrix[1,4]=1.0;
-    Amatrix[2,5]=1.0;
-
-    expr2 = 3*(AstCnst.mu_E/r**5)*(1 - 2.5*AstCnst.J_2*(AstCnst.R_E/r)**2*(7*(X[2]/r)**2) -1); # fixed
-    Amatrix[3,0] = expr1 + expr2*X[0]**2; 
-    Amatrix[3,1] = expr2*X[0]*X[1]; 
-    expr3 =  3*(AstCnst.mu_E/r**5)*(1 - 2.5*AstCnst.J_2*(AstCnst.R_E/r)**2*(7*(X[2]/r)**2) -3);
-    Amatrix[3,2] = expr3*X[0]*X[2];
+    P = np.reshape(X[6:],(6,6));
     
-    Amatrix[4,0] = Amatrix[3,1];
-    Amatrix[4,1] = expr1 + expr2*X[1]**2;
-    Amatrix[4,2] = expr3*X[1]*X[2];
-
-    Amatrix[5,0] = Amatrix[3,2];
-    Amatrix[5,1] = Amatrix[4,2];
-    Amatrix[5,2] =-(AstCnst.mu_E/r**3)*(1-expr*(5*(X[2]/r)**2 -3)) + 3*(AstCnst.mu_E/r**5)*(1 - 2.5*AstCnst.J_2*(AstCnst.R_E/r)**2*(7*(X[2]/r)**2) -5)*X[2]**2;
-
-    P = np.dot(Amatrix,P) + np.transpose(np.dot(P,Amatrix)) + np.dot(np.dot(L,Q),np.transpose(L));
+    # Find matrix A (state sensitivity matrix)
+    Amatrix = fnJacobian_Kepler_J2(X);
+    # Covariance matrix propagation equation.
+    P = np.dot(Amatrix,P) + np.dot(P,np.transpose(Amatrix)) + np.dot(np.dot(L,Q),np.transpose(L));
+    
     # to force symmetry for covariance matrix P.
     P = 0.5*(P + np.transpose(P));
-    
-    Xdot[6:6+6*6+1] = np.reshape(P,6*6);
+    # and positive definiteness
+    S,definite = MathsFn.schol(P);
+    P = np.dot(S,S.T);
+
+    # Pop covariance matrix into state vector
+    Xdot[6:] = np.reshape(P,6*6);
     return Xdot;
 
 def fnJacobian_Kepler_J2(X):
+    # fnJacobian_Kepler_J2 evaluates the Jacobian matrix of function fnKepler_J2 at X.
+    
     r = np.linalg.norm(X[0:3]);
     expr = 1.5*AstCnst.J_2*(AstCnst.R_E/r)**2 ;
     expr1 = -(AstCnst.mu_E/r**3)*(1 - expr*(5*((X[2]/r)**2) -1));
@@ -210,6 +188,4 @@ def fnJacobian_Kepler_J2(X):
 
 
     
-
-
     
